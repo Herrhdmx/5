784cff18ef034c7dbe2e05c9e2ef52bdcb27b239
Hermann L <hermann252000@gmx.de>
2021-11-17 19:38:33 +0100

    added commentary

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -1,10 +1,20 @@
 package de.hfu;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+/**
+ * Application class for OSSE - Praktikum 5: Build Management
+ * @author Hermann Lau
+ * @version 1.0
+ */
 
 public class App {
+
+    /**
+     * This program reads the user console input and prints it back capitalized.
+     */
+
     public static void main(String[] args) throws IOException {
         String read_it = "";
         System.out.print("Give me text: ");
